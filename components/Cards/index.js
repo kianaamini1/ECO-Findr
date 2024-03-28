fdc19a3afc79cb39099961437d8b80d302ea043f
kianaamini1 <kamini4@my.bcit.ca>
2024-03-28 12:40:32 -0700

    adding cards and chip components

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,3 +1,14 @@
-import styles from './components/Cards/cards.modules.css'
 
+import styles from "./cards.module.css";
 
+export default function Card({ title, imgSrc, alt }) {
+    
+  return (
+    <div className={styles.Cards}>
+      <img src={imgSrc} alt={alt} />
+      <div className={styles.bottomTitle}>
+        <h2>{title}</h2>
+      </div>
+    </div>
+  );
+}
--- components/Chips/index.js
+++ components/Chips/index.js
@@ -2,7 +2,7 @@ import styles from './cards.modules.css'
 
 export default function Chip(buttonText) {
     return (
-        <button type="submit" class="chip-card">
+        <button type="submit" className={styles.chipCard}>
             {buttonText}
         </button>
     )
